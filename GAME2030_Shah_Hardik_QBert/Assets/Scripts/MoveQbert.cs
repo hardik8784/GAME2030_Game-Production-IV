c59b327a9d7295e5bfdf96b332c254ecb23a1e16
Hardik <hardik8784@gmail.com>
2021-01-28 13:13:04 -0500

    Initial Commit for Game Production IV

---
+++ GAME2030_Shah_Hardik_QBert/Assets/Scripts/MoveQbert.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveQbert : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("7"))
+        {
+            GetComponent<Rigidbody>().velocity = new Vector3(0, 4, -1);
+        }
+
+        if (Input.GetKeyDown("9"))
+        {
+            GetComponent<Rigidbody>().velocity = new Vector3(0, 6, 1);
+        }
+
+        if (Input.GetKeyDown("1"))
+        {
+            GetComponent<Rigidbody>().velocity = new Vector3(1, 4, 0);
+        }
+
+        if (Input.GetKeyDown("3"))
+        {
+            GetComponent<Rigidbody>().velocity = new Vector3(-1, 6, 0);
+        }
+
+
+    }
+}
