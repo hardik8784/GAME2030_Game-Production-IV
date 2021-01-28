49160e9b8c25b74e09d9d9704a71af22c47841f1
Hardik <hardik8784@gmail.com>
2021-01-28 15:12:38 -0500

    Functionality Achieved -->When Qbert Land on the Cube it change colour

---
+++ GAME2030_Shah_Hardik_QBert/Assets/Scripts/CubeControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeControl : MonoBehaviour
+{
+    public Material YellowColour;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        GetComponent<Renderer>().material = YellowColour;
+    }
+}
