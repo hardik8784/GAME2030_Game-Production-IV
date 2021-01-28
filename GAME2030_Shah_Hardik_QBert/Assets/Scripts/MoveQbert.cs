2e07cc7b143fe1ea0b721bc65ea24e1357215240
Hardik <hardik8784@gmail.com>
2021-01-28 14:51:07 -0500

    Added Red Ball,Green Ball,Character Movement & Coily

--- GAME2030_Shah_Hardik_QBert/Assets/Scripts/MoveQbert.cs
+++ GAME2030_Shah_Hardik_QBert/Assets/Scripts/MoveQbert.cs
@@ -13,23 +13,27 @@ public class MoveQbert : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown("7"))
+        if(Input.GetKeyDown("1"))
         {
+            transform.eulerAngles = new Vector3(0, -180 , 0);
             GetComponent<Rigidbody>().velocity = new Vector3(0, 4, -1);
         }
 
         if (Input.GetKeyDown("9"))
         {
+            transform.eulerAngles = new Vector3(0, -360, 0);
             GetComponent<Rigidbody>().velocity = new Vector3(0, 6, 1);
-        }
-
-        if (Input.GetKeyDown("1"))
-        {
-            GetComponent<Rigidbody>().velocity = new Vector3(1, 4, 0);
         }
 
         if (Input.GetKeyDown("3"))
         {
+            transform.eulerAngles = new Vector3(0, -270, 0);
+            GetComponent<Rigidbody>().velocity = new Vector3(1, 4, 0);
+        }
+
+        if (Input.GetKeyDown("7"))
+        {
+            transform.eulerAngles = new Vector3(0, -90, 0);
             GetComponent<Rigidbody>().velocity = new Vector3(-1, 6, 0);
         }
 
