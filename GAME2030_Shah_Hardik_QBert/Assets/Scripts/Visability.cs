7aeae6259b31d3ec770ce9328d943b993ffa0792
Hardik <hardik8784@gmail.com>
2021-02-03 11:19:41 -0500

    Updated Milestone 1 with smaller UI and perfect balance of the gameplay

---
+++ GAME2030_Shah_Hardik_QBert/Assets/Scripts/Visability.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Visability : MonoBehaviour
+{
+    SpriteRenderer rend;
+    
+   // public float targetTime = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = this.gameObject.GetComponent<SpriteRenderer>();
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //targetTime += Time.deltaTime;
+       
+        //if (targetTime > 0.0f)
+        //{
+            rend.enabled = !rend.enabled;
+        //}
+    }
+}
+
